b0ef40d6494ed63dea5ff5d300af7a065973f671
c0venn <fabichidori2@gmail.com>
2021-06-17 01:27:42 +0000

    commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,27 +1,21 @@
 import React, { useState } from "react";
 
-let [List, setList] = useState([]);
-let [ItemNumber, setItemNumber] = useState(List.length);
-
-const addItem = e => {
-	setList(List.concat({ label: e.target.value }));
-	e.target.value = "";
-	setItemNumber(ItemNumber + 1);
-};
-
 export function Home() {
+	let [List, setList] = useState([]);
+	let [ItemNumber, setItemNumber] = useState(List.length);
+
+	const addItem = () => {
+		<h1>hola</h1>;
+	};
+
 	return (
 		<div className="text-center mt-5">
 			<h1>To Do List</h1>
-			<form>
-				<input
-					type="text"
-					placeholder="What's need to be done?"></input>
-				<button type="button" onClick={addItem}>
-					Add
-				</button>
-				<button type="button">Delete</button>
-			</form>
+			<input type="text" placeholder="What's need to be done?"></input>
+			<button type="button" onClick={addItem}>
+				Add
+			</button>
+			<button type="button">Delete</button>
 			<small>{ItemNumber} Item Left</small>
 		</div>
 	);
